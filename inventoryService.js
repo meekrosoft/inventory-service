8e8e9d77c5ba78541fe2d0f1fbeabad0119fbbe4
Johnny Tu <johnny.tu@servicerocket.com>
2015-05-30 03:05:36 -0400

    changed inventoryService to use standard redis host and port

--- inventoryService.js
+++ inventoryService.js
@@ -1,5 +1,5 @@
 var redis = require('redis');
-var client = redis.createClient(6379, "redisdb");
+var client = redis.createClient();
  
 client.on('connect', function() {
     console.log('connected');
